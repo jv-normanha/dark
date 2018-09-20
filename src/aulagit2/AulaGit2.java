1961be65e51ff663fd7eab39180ff2fd8349e8ac
Riverio <riveriosantiago@gmail.com>
2018-09-19 21:16:11 -0300

    segundo auteração

--- src/aulagit2/AulaGit2.java
+++ src/aulagit2/AulaGit2.java
@@ -16,7 +16,7 @@ public class AulaGit2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World!");
+        System.out.println(" World!");
         System.out.println("AulasssGitsss");
         System.out.println("MARCELO HZELA");
         
